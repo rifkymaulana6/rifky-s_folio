e02ad5c5a0f46ca0253eecfa3ede81fb3d8a3a64
RIFKY MAULANA <rifkymaulana0612@gmail.com>
2023-09-10 16:31:52 +0700

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Rifky Maulana </span> Since 2020 I started studying as a Software Engineer at one of the campuses in Bandung. Now I am interested in becoming a data scientist, data analyst, or data engineer. In college, I learned many programming languages and frameworks. But what I like the most are the lessons on machine learning and data processing. 🙂 <br> <br> Apart from machine learning, I also like creating software such as websites and mobile applications. Especially on the backend, because I'm not good at design and UI/UX.",
+    ],
+    typeSpeed: 5,
+    contentType: 'html',
+  });
+}, 100);

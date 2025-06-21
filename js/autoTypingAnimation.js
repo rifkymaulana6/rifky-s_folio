827c837cd999401b7dbf958ed4b4f3c193827f6d
RIFKY MAULANA <rifkymaulana0612@gmail.com>
2025-06-21 10:36:24 +0700

    fix about me and hero image

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,9 +1,9 @@
 setTimeout(function () {
   $('.about-info-inner').typed({
     strings: [
-      "<span>Hi, I am Rifky Maulana </span> Since 2020 I started studying as a Software Engineer at one of the campuses in Bandung. Now I am interested in becoming a data scientist, data analyst, or data engineer. In college, I learned many programming languages and frameworks. But what I like the most are the lessons on machine learning and data processing. 🙂 <br> <br> Apart from machine learning, I also like creating software such as websites and mobile applications. Especially on the backend, because I'm not good at design and UI/UX.",
+      "<span>Hi, I am Rifky Maulana, I am a bachelor of informatics engineering. </span> Since 2020 I started studying as a Software Engineer at one of the campuses in Bandung. Now I am interested in becoming a data scientist, data analyst, data engineer, or other fields related to data. In college, I learned many programming languages and frameworks. But what I like the most are the lessons on machine learning and data processing. 🙂 <br> <br> Apart from machine learning, I also like creating software such as websites and mobile applications. Especially on the backend, because I'm not good at design and UI/UX.",
     ],
-    typeSpeed: 5,
+    typeSpeed: 1,
     contentType: 'html',
   });
 }, 100);
